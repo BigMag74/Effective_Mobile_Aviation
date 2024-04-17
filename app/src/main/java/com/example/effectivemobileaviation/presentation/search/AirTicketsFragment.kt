9b2f3b68d9dfd9cd27c7bfc47110844f2edde342
Vladislav <bigmag174@yandex.ru>
2024-04-17 17:13:29 +0500

    Добавил возможность вводить только кириллицу

--- app/src/main/java/com/example/effectivemobileaviation/presentation/search/AirTicketsFragment.kt
+++ app/src/main/java/com/example/effectivemobileaviation/presentation/search/AirTicketsFragment.kt
@@ -1,6 +1,19 @@
 package com.example.effectivemobileaviation.presentation.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.effectivemobileaviation.databinding.FragmentAirTicketsBinding
 
 class AirTicketsFragment : Fragment() {
+
+    private var _binding: FragmentAirTicketsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentAirTicketsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
